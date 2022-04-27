2084cfa7b3b7800c084feac6cd6c4a8615a4f678
GoodEnoughL <betterhgl@gmail.com>
2022-04-27 10:17:22 +0800

    bug(waresTrigger): 修复触发器中时区时间戳问题

--- cloudfunctions/waresTrigger/index.js
+++ cloudfunctions/waresTrigger/index.js
@@ -4,14 +4,21 @@ cloud.init({
 })
 const db = cloud.database();
 const _ = db.command
+function getLocaleDateZone() {
+  return new Date(new Date().toDateString()).getTime()
+}
+function toUTC8DateZone(utc0) {
+  return utc0 - 28800000
+}
 exports.main = async (event, context) => {
   try {
+    const today = toUTC8DateZone(getLocaleDateZone())
     db.collection("wares").add({
-        data:{"alias":"篮球馆","bussdate":1.6510752E+12,"name":"basket","record":[{"busstime":68400,"equipment":{"A1":[]}},{"busstime":70200,"equipment":{"A1":[]}},{"busstime":72000,"equipment":{"A1":[]}},{"busstime":73800,"equipment":{"A1":[]}}],"stock":[{"busstime":68400,"equipment":"A1","surplus":2},{"busstime":70200,"equipment":"A1","surplus":2},{"busstime":72000,"equipment":"A1","surplus":2},{"busstime":73800,"equipment":"A1","surplus":2}],"equipment":["A1"]}
+        data:{"alias":"篮球馆","bussdate":today,"name":"basket","record":[{"busstime":68400,"equipment":{"A1":[]}},{"busstime":70200,"equipment":{"A1":[]}},{"busstime":72000,"equipment":{"A1":[]}},{"busstime":73800,"equipment":{"A1":[]}}],"stock":[{"busstime":68400,"equipment":"A1","surplus":2},{"busstime":70200,"equipment":"A1","surplus":2},{"busstime":72000,"equipment":"A1","surplus":2},{"busstime":73800,"equipment":"A1","surplus":2}],"equipment":["A1"]}
     })
-    return 
+    return "success"
   }
   catch(e) {
-    return e
+    return ["fail:",e]
   }
 };
---
+++ miniprogram/util/index.js
@@ -0,0 +1,17 @@
+
+function getHMData(base,timestamp){
+  const date = new Date(base+timestamp * 1000) // 时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    const h = `${date.getHours()}:`
+    const m = `${date.getMinutes()}`
+    // s = date.getSeconds()
+    return h + m
+}
+
+function getLocaleDateZone() {
+  return new Date(new Date().toDateString()).getTime()
+}
+
+module.default = {
+  getHMData,
+  getLocaleDateZone
+}
