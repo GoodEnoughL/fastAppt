a36a9a5cfbb6a7882d2fc0a6b92cc7508bfb8921
GoodEnoughL <betterhgl@gmail.com>
2022-05-05 01:40:58 +0800

    feat(waresTrigger):定时器删除过期库存

--- cloudfunctions/waresTrigger/index.js
+++ cloudfunctions/waresTrigger/index.js
@@ -11,14 +11,20 @@ function toUTC8DateZone(utc0) {
   return utc0 - 28800000
 }
 exports.main = async (event, context) => {
+  const result = {status:'fail'}
   try {
     const today = toUTC8DateZone(getLocaleDateZone())
     db.collection("wares").add({
         data:{"alias":"篮球馆","bussdate":today,"name":"basket","record":[{"busstime":68400,"equipment":{"A1":[]}},{"busstime":70200,"equipment":{"A1":[]}},{"busstime":72000,"equipment":{"A1":[]}},{"busstime":73800,"equipment":{"A1":[]}}],"stock":[{"busstime":68400,"equipment":"A1","surplus":2},{"busstime":70200,"equipment":"A1","surplus":2},{"busstime":72000,"equipment":"A1","surplus":2},{"busstime":73800,"equipment":"A1","surplus":2}],"equipment":["A1"]}
     })
-    return "success"
+    db.collection("wares").where({
+      bussdate: _.lt(today)
+    }).remove()
+    result.status = 'success'
+    return result
   }
   catch(e) {
-    return ["fail:",e]
+    result.reason = e
+    return result
   }
 };
